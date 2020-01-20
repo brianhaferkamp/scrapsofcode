875fbe4a3e03ee90cc2df7e427f6ae67152e1716
Brian Haferkamp <brian.haferkamp@gmail.com>
2020-01-20 14:59:31 -0600

    Update script.js

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -1,19 +1,27 @@
-function loadSharingLink() {  
-  var copiedText = $(this).attr('href');
-  console.log(copiedText);
-  var currShareBox = $(this).parents('.article-meta').siblings('.article-share-link');
-  console.log(currShareBox);
-  var currShareBoxInput = $(this).parents('.article-meta').siblings('.article-share-link');
-  console.log(currShareBoxInput);
-  currShareBoxInput.find('input').val(copiedText);
-  currShareBox.slideDown();
-}
+$(document).ready(function() {
+  $('.article-link').on('click', function(e) {
+    e.preventDefault();
+    loadSharingLink();
+  });
 
 
-$('.article-link').on('click', function(e) {
-  e.preventDefault();
-  loadSharingLink();
+  function loadSharingLink() {
+    var copiedText = $(this).attr('href');
+    console.log(copiedText);
+    var currShareBox = $(this).parents('.article-meta').siblings('.article-share-link');
+    console.log(currShareBox);
+    var currShareBoxInput = $(this).parents('.article-meta').siblings('.article-share-link');
+    console.log(currShareBoxInput);
+    currShareBoxInput.find('input').val(copiedText);
+    currShareBox.slideDown();
+  }
+
 });
+
+
+
+
+
 
 
 // $('.article-share-link i').on('click', function() {
