73617c7ab333b54dedce10466004340ab4ebc1c7
Brian Haferkamp <brian.haferkamp@gmail.com>
2020-01-20 15:22:23 -0600

    Update script.js

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -1,30 +1,25 @@
-if (Mavo.dataLoaded) {
-  console.log('Data loaded');
+$('.article-link').on('click', function() {
+  loadSharingLink();
+});
+
+
+function loadSharingLink() {
+  var copiedText = $(this).attr('href');
+  console.log(copiedText);
+  var currShareBox = $(this).parents('.article-meta').siblings('.article-share-link');
+  console.log(currShareBox);
+  var currShareBoxInput = $(this).parents('.article-meta').siblings('.article-share-link');
+  console.log(currShareBoxInput);
+  currShareBoxInput.find('input').val(copiedText);
+  currShareBox.slideDown();
 }
 
-// $('.article-link').on('click', function(e) {
-//   e.preventDefault();
-//   loadSharingLink();
-// });
-
-
-// function loadSharingLink() {
-//   var copiedText = $(this).attr('href');
-//   console.log(copiedText);
-//   var currShareBox = $(this).parents('.article-meta').siblings('.article-share-link');
-//   console.log(currShareBox);
-//   var currShareBoxInput = $(this).parents('.article-meta').siblings('.article-share-link');
-//   console.log(currShareBoxInput);
-//   currShareBoxInput.find('input').val(copiedText);
-//   currShareBox.slideDown();
-// }
 
 
 
 
 
 
-
-// $('.article-share-link i').on('click', function() {
-//   $('.article-share-link').slideUp();
-// });
+$('.article-share-link i').on('click', function() {
+  $('.article-share-link').slideUp();
+});
