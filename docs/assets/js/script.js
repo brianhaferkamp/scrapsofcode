981b134e01a7501c4bdbdcfbee92632dd5645232
Brian Haferkamp <brian.haferkamp@gmail.com>
2020-01-20 14:29:38 -0600

    Update script.js

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -1,8 +1,11 @@
 $('.article-link').on('click', function(e) {
   e.preventDefault();
   var copiedText = $(this).attr('href');
+  console.log(copiedText);
   var currShareBox = $(this).parents('.article-meta').siblings('.article-share-link');
+  console.log(currShareBox);
   var currShareBoxInput = $(this).parents('.article-meta').siblings('.article-share-link');
+  console.log(currShareBoxInput);
   currShareBoxInput.find('input').val(copiedText);
   currShareBox.slideDown();
 });
