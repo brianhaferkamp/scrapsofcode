3f41d7f8f4362535ff035740c92e6b89b4c4b67b
Brian Haferkamp <brian.haferkamp@gmail.com>
2020-01-20 15:09:17 -0600

    Update script.js

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -1,22 +1,22 @@
-if (Mavo.dataLoaded) {
-  console.log('All Data Loaded');
-}
+Mavo.inited
+    .then(() => Mavo.all[0].dataLoaded)
+    .then(() => Mavo.all[0].render({"foo": ["e", "f", "g", "h", "i", "j"]}));
 
-$('.article-link').on('click', function(e) {
-  e.preventDefault();
-  loadSharingLink();
-});
+// $('.article-link').on('click', function(e) {
+//   e.preventDefault();
+//   loadSharingLink();
+// });
 
 
-function loadSharingLink() {
-  var copiedText = $(this).attr('href');
-  console.log(copiedText);
-  var currShareBox = $(this).parents('.article-meta').siblings('.article-share-link');
-  console.log(currShareBox);
-  var currShareBoxInput = $(this).parents('.article-meta').siblings('.article-share-link');
-  console.log(currShareBoxInput);
-  currShareBoxInput.find('input').val(copiedText);
-  currShareBox.slideDown();
+// function loadSharingLink() {
+//   var copiedText = $(this).attr('href');
+//   console.log(copiedText);
+//   var currShareBox = $(this).parents('.article-meta').siblings('.article-share-link');
+//   console.log(currShareBox);
+//   var currShareBoxInput = $(this).parents('.article-meta').siblings('.article-share-link');
+//   console.log(currShareBoxInput);
+//   currShareBoxInput.find('input').val(copiedText);
+//   currShareBox.slideDown();
 }
 
 
