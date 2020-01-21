f11fbe48a2676e9e00987bb7f1baf45f205d35f0
Brian Haferkamp <brian.haferkamp@gmail.com>
2020-01-21 10:56:48 -0600

    Update script.js

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -1,16 +1,9 @@
-// $('.article-link').on('click', function() {
-//   var $this = $(this);
-//   var copiedText = $this.attr('data-link');
-// //   var copiedText = $(this).attr('data-link');
-// //   console.log(copiedText);
-// //   var currShareBox = $(this).parents('.article-meta').siblings('.article-share-link');
-// //   console.log(currShareBox);
-// //   var currShareBoxInput = $(this).parents('.article-meta').siblings('.article-share-link');
-// //   console.log(currShareBoxInput);
-// //   currShareBoxInput.find('input').val(copiedText);
-// //   currShareBox.slideDown();
-// });
+$('.article-link').on('click', function() {
+  var currShareBox = $(this).parents('.article-meta').siblings('.article-share-link');
+  console.log(currShareBox);
+  currShareBox.slideDown();
+});
 
-// $('.article-share-link i').on('click', function() {
-// //   $('.article-share-link').slideUp();
-// });
+$('.article-share-link i').on('click', function() {
+  $('.article-share-link').slideUp();
+});
